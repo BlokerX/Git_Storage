b5e3f9338eea364e73edebc80be13967f4c6e71a
BlokerX <jakubmichalik0000@gmail.com>
2025-01-09 16:42:35 +0100

    SMS32: Device interruption

--- Samples/SMS32_Assembler_Symulator/Commands_For_SMS32.asm
+++ Samples/SMS32_Assembler_Symulator/Commands_For_SMS32.asm
@@ -11,6 +11,7 @@ Flagi:
     I - Przerwanie
 
 Instrukcje podstawowe:
+
     MOV AL,0 - Przenoszenie wartości do rejestru 
 
     ADD AL,1 - Dodaje do rejestru
@@ -27,10 +28,15 @@ Instrukcje podstawowe:
                 S - jeżeli wartość rejestru jest mniejsza (AL<3)
 
 Instrukcje stosu:
+
     PUSH AL - Kopiuje z rejestru na stos
     POP AL  - Zdejmuje wartość ze stosu
 
+    PUSHF - Kopiuje flagi na stos
+    POPF - Zdejmuje wartość flagi ze stosu
+
 Instrukcje skoku (pętle itd):
+
     label:    - Etykieta o nazwie label
 
     JMP label - Skok bezwarunkowy
@@ -45,6 +51,7 @@ Instrukcje skoku (pętle itd):
     JNO label - Skok jeśli nie przepełnione
 
 Tablica (szablon kodu):
+
     JMP start - poczatek tablicy (pomija wykonanie niedozwolonej operacji)
     DB 1      - wartość 1
     DB 2      - wartość 2
@@ -52,6 +59,7 @@ Tablica (szablon kodu):
     start:    - koniec tablicy
 
 Procedura (szablon kodu):
+
     ORG A   - Procedura o adresie A
     RET     - Powraca do CALL / Kończy procedurę
 
@@ -74,13 +82,52 @@ Procedura (szablon kodu):
 Przerwanie programowe (szablon kodu):
 
     ORG 03 ;  - Przerwanie programowe
-      DB 4B ; - wywołanie przerwania o numerze 03 jako procedura pod adresem 4B
+      DB 4B ; - Wywołanie przerwania o numerze 03 jako procedura pod adresem 4B
 
     ORG 4B ;  - Procedura przerwania programowego o adresie 4B
       ;(kod [przerwania])
       IRET ;  - Powrót z przerwania programowego (Interruption Return)
 
     INT 03 - Wywołanie przerwania o numerze 03
+
+Przerwania sprzętowe:
+
+    DB 10 - Wektor przerwania zegara (Przerwanie 02)
+    DB 00 - Wektor przerwania klawiatury (Przerwanie 03)
+    DB 20 - Wektor przerwania klawiatury numerycznej (Przerwanie 04)
+
+    NOP - nic nie robi (not operation)
+
+    CLI - Czyści flagę I na 0 czyli wyłącza przerwania
+
+    STI - Ustawia flagę I na 1 czyli włącza przerwania
+
+    PUSHF - ODKŁADA Z FLAGAMI NA STOS
+
+Przerwanie sprzętowe (szablon kodu):
+
+    JMP start ; - Skok za tablice wektorow przerwań
+
+    DB 10 ; - Wektor przerwania zegara
+    DB 00 ; - Wektor przerwania klawiatury (nieuzywany)
+    DB 20 ; - Wektor przerwania klawiatury numerycznej
+
+
+    ORG 10
+     CLI ; - Wyłączenie przerwań
+      PUSH AL
+       PUSHF
+        ;kod przerwana
+       POPF
+      POP AL
+     STI ; - Włączenie przerwań
+    IRET
+
+    start:
+    STI ; Włączenie przerwań
+     ;kod programu
+    END
+
 
 Komórki wyświetlacza VDU (konsoli) [64 bity]:
     C0 - pierwszy znak
@@ -114,9 +161,11 @@ Urządzenia (lista):
     128bit-C1, 64bit-Ż1, 32bit-Z1, 16bit-C2; 8bit-Ż2, 4bit-Z2, 2bit-X, 1bit-X
 
 Wyświetlacz siedmiosegmentowy:
+
     Pierwszy bit (1):
         0 - Lewy wyświetlacz
         1 - Prawy wyświetlacz
+
     Drugi bit (2)   - Prawa górna krawędź
     Drugi bit (4)   - Środkowa krawędź
     Drugi bit (8)   - Prawa dolna krawędź
@@ -125,15 +174,61 @@ Wyświetlacz siedmiosegmentowy:
     Drugi bit (64)  - Lewa górna krawędź
     Drugi bit (128) - Górna krawędź
     
-    PRZYKŁAD:
+    PRZYKŁADY:
     ---
     MOV AL, 0 OUT 02 - Wyzerowanie lewego
     MOV AL, 1 OUT 02 - Wyzerowanie prawego
     ---
 
-Grzałka i termostat: - Dość skomplikowane -
+    MOV AL, FA OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 0 
+    MOV AL, 0A OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 1
+    MOV AL, B6 OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 2
+    MOV AL, 9E OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 3
+    MOV AL, 4E OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 4
+    MOV AL, DC OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 5
+    MOV AL, FC OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 6 
+    MOV AL, 8A OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 7
+    MOV AL, FE OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 8
+    MOV AL, DE OUT 01 ; Ustawia wyświetlacz siedmiosegmentowy (lewy) na 9
+
+    MOV AL, FB OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 0
+    MOV AL, 0B OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 1
+    MOV AL, B7 OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 2
+    MOV AL, 9F OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 3
+    MOV AL, 4F OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 4
+    MOV AL, DD OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 5
+    MOV AL, FD OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 6 
+    MOV AL, 8B OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 7
+    MOV AL, FF OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 8
+    MOV AL, DF OUT 02 ; Ustawia wyświetlacz siedmiosegmentowy (prawy) na 9
+
+    ---
+
+Grzałka i termostat (automatyczna kontrola temperatury w normie - PRZYKŁAD kodu):
+
+    MOV AL, 0
+    OUT 03
+
+    PETLA:
+    IN 03
+    AND AL, 1
+    JZ HOT
+    JNZ COLD
+
+    COLD: ; Schładzanie
+    MOV AL, 00
+    OUT 03
+    JMP PETLA
+
+    HOT: ; Ogrzewanie
+    MOV AL, 80
+    OUT 03
+    JMP PETLA
+
+    END
 
 Labirynt:
+
     Bity:
         4 najmniej znaczące     - Dystans
         4 najbardziej znaczące  - Kierunek
@@ -148,6 +243,7 @@ Silnik krokowy:
     Magnesy krokowe 8, 4, 2, 1
 
 Dane z klawiatury:
+
     IN 00 - Czekaj na naciśnięcie klawisza i zapisz jego kod (ASCI) w AL
 
     CMP AL, 0D - Sprawdź czy wpisanym klawiszem był enter (0D hex)
